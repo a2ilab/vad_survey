cc25cd3df197725221d57a42bbb40826985bddd0
Applied AI Lab <a2i.kmou@gmail.com>
2025-04-21 17:42:27 +0900

    settings.py

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 # static 파일 설정
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
